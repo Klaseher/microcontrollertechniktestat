136c9d45dc443cf6b7042f5e4203c617ba188850
Paul Buschmann <paul.buschmann@gmx.de>
2022-12-14 14:32:23 +0100

    changed DATA_DIO to DATENPORT_B

--- testat_2_ino.c/testat_2_ino.c.ino
+++ testat_2_ino.c/testat_2_ino.c.ino
@@ -5,15 +5,15 @@ void init()
   /* Set DIO pins to outputs */
   pinMode(ANZEIGEPORT_D,OUTPUT);
   pinMode(TAKTPORT_D,OUTPUT);
-  pinMode(DATA_DIO,OUTPUT);
+  pinMode(DATENPORT_B,OUTPUT);
 } 
  
 /* Write a decimal number between 0 and 9 to one of the 4 digits of the display */
 void WriteNumberToSegment(byte Segment, byte Value)
 {
   digitalWrite(ANZEIGEPORT_D,LOW);
-  shiftOut(DATA_DIO, TAKTPORT_D, MSBFIRST, ~numbers[Value]);
-  shiftOut(DATA_DIO, TAKTPORT_D, MSBFIRST, stellen[Segment]);
+  shiftOut(DATENPORT_B, TAKTPORT_D, MSBFIRST, ~numbers[Value]);
+  shiftOut(DATENPORT_B, TAKTPORT_D, MSBFIRST, stellen[Segment]);
   digitalWrite(ANZEIGEPORT_D,HIGH);
 }
 
@@ -26,7 +26,7 @@ int main()
     WriteNumberToSegment(0 , 0);
     WriteNumberToSegment(1 , 1);
     WriteNumberToSegment(2 , 2);
-    WriteNumberToSegment(3 , 3);
+    WriteNumberToSegment(3 , 10);
     
   }
   
